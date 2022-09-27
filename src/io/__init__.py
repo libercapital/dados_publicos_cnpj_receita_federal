e2d4830a61ac856afdc5b19794fdd90fea97fa21
André Tayer <andre.tayer@libercapital.com.br>
2022-09-27 15:07:27 -0300

    refactor: add headers on requests

--- src/io/__init__.py
+++ src/io/__init__.py
@@ -6,3 +6,7 @@ QUAL_SOCIO_JSON_NAME = 'qual_socio.json'
 MOTIVOS_JSON_NAME = 'motivos.json'
 PAIS_JSON_NAME = 'pais.json'
 MUNIC_JSON_NAME = 'municipios.json'
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+}
