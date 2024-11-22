acd5a2f6917929f8c242dd8732c4a1197e8b526a
Robson <contato.robsonallef@gmail.com>
2024-11-22 14:04:05 -0300

    fix: fix how files are downloaded

--- src/io/get_last_ref_date.py
+++ src/io/get_last_ref_date.py
@@ -1,11 +1,8 @@
-from collections import Counter
-from datetime import datetime
-
 import requests
 from bs4 import BeautifulSoup
-from requests.adapters import HTTPAdapter, Retry
 
-from src.io import CORE_URL_FILES, HEADERS
+from src.io import CORE_URL_FILES
+from src.io import HEADERS
 
 
 def main():
@@ -14,11 +11,11 @@ def main():
     :return: dict with urls from files as well as last modified date and size in bytes
     """
     # get page content
-    page = requests.get(CORE_URL_FILES, headers=HEADERS)
+    _folder_open_date = 'dados_abertos_cnpj'
+    page = requests.get(f'{CORE_URL_FILES}/{_folder_open_date}', headers=HEADERS)
 
     # BeautifulSoup object
     soup = BeautifulSoup(page.text, 'html.parser')
-
     table = soup.find('table')
     rows = table.find_all('tr')
     list_last_modified_at = []
@@ -26,16 +23,13 @@ def main():
     print('creating dict files url')
     for row in rows:
         if row.find_all('td'):
-            if row.find_all('td')[1].find('a')['href'].endswith('.zip'):
+            if row.find_all('td')[1].find('a')['href'].replace('-', '').replace('/', '').isdigit():
                 # get last modified time and parse to date (ex: '2021-07-19')
-                list_last_modified_at.append(
-                    datetime.strptime(row.find_all('td')[2].text.strip(), '%Y-%m-%d %H:%M').strftime(
-                        '%Y-%m-%d'))
-
+                list_last_modified_at.append(row.find_all('td')[1].find('a')['href'].replace('/', ''))
     # get the most common on 'last_modified' from source
-    ref_date, occurences = Counter(list_last_modified_at).most_common(1)[0]
-    print(
-        f"ref date will be: '{ref_date}' with {occurences} out of {len(list_last_modified_at)} ({occurences / len(list_last_modified_at):.1%}) ")
+    ref_date = max(list_last_modified_at)
+    print('last updated date is ', ref_date)
+
     return ref_date
 
 
